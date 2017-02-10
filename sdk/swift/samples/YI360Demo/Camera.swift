9348f5fab312ec181289c414e70ba8122c4f25d0
JaviLorbada <javugi@gmail.com>
2017-02-10 10:01:56 +0100

    Remove all warnings, especially the ones about unused result and remove all `;` to make more swift-ish.

--- sdk/swift/samples/YI360Demo/Camera.swift
+++ sdk/swift/samples/YI360Demo/Camera.swift
@@ -15,141 +15,141 @@
 // THIS SOFTWARE OR ITS DERIVATIVES.
 //=============================================================================
 
-import Foundation;
-import YICameraSDK;
+import Foundation
+import YICameraSDK
 
 protocol CameraListener {
-    func onStateChanged(camera: Camera, newState: Camera.State, oldState: Camera.State);
+    func onStateChanged(camera: Camera, newState: Camera.State, oldState: Camera.State)
 }
 
 class Camera: ActionCameraListener
 {
     enum State
     {
-        case Disconnected;
-        case Connecting;
-        case Connected;
-        case StartRecording;
-        case Recording;
+        case Disconnected
+        case Connecting
+        case Connected
+        case StartRecording
+        case Recording
     }
     
     class DQ: YICameraSDKDispatchQueue
     {
         @objc func dispatch(task: @escaping () -> ()) {
             DispatchQueue.main.async {
-                task();
+                task()
             }
         }
     }
 
-    private var mState = State.Disconnected;
-    private var mListener: CameraListener?;
-    private var mIP = "";
-    private var mHost = "";
-    private var mYICamera: ActionCamera!;
-    private var mTimer: Timer?;
+    private var mState = State.Disconnected
+    private var mListener: CameraListener?
+    private var mIP = ""
+    private var mHost = ""
+    private var mYICamera: ActionCamera!
+    private var mTimer: Timer?
     
     init(ip: String, host: String) {
-        super.init();
-        mIP = ip;
-        mHost = host;
-        mYICamera = ActionCamera(listener: self, dispatchQueue: DQ());
+        super.init()
+        mIP = ip
+        mHost = host
+        mYICamera = ActionCamera(listener: self, dispatchQueue: DQ())
     }
     
     func setListener(listener: CameraListener?) {
-        mListener = listener;
+        mListener = listener
     }
     
     // Connect to YIActionCamera. Due to SDK doesn't support timeout in connect() currently,
     // we need handle timeout by our self.
     func connect() {
-        setState(state: .Connecting);
-        mTimer = Timer.scheduledTimer(timeInterval: 5, target: self, selector: #selector(onConnectTimeout), userInfo: nil, repeats: false);
-        mYICamera.connect(connectionString: "tcp:\(mIP):7878");
+        setState(state: .Connecting)
+        mTimer = Timer.scheduledTimer(timeInterval: 5, target: self, selector: #selector(onConnectTimeout), userInfo: nil, repeats: false)
+        mYICamera.connect(connectionString: "tcp:\(mIP):7878")
     }
     
     func startRecording(startTime: NSDate) {
         if (mState == .Connected) {
-            setState(state: .StartRecording);
-            mYICamera.stopRecording(success: nil, fail: nil)
+            setState(state: .StartRecording)
+            _ = mYICamera.stopRecording(success: nil, fail: nil)
                      .setSystemMode(mode: .Record, success: nil, fail: nil)
                      .setDateTime(
                         datetime: NSDate() as Date,
                         success: {
-                            self.mYICamera.startRecording(hour: startTime.getHours(), minute: startTime.getMinutes(), second: startTime.getSeconds(),
+                            _ = self.mYICamera.startRecording(hour: startTime.getHours(), minute: startTime.getMinutes(), second: startTime.getSeconds(),
                                 success: nil,
                                 fail: { error in
-                                    print("start recording failed: \(error.getCode())");
+                                    print("start recording failed: \(error.getCode())")
                                     if (self.mState == .StartRecording) {
-                                        self.setState(state: .Connected);
+                                        self.setState(state: .Connected)
                                     }
-                            });
+                            })
                         },
                         fail: { error in
-                            print("set datetime failed: \(error.getCode())");
+                            print("set datetime failed: \(error.getCode())")
                             if (self.mState == .StartRecording) {
-                                self.setState(state: .Connected);
+                                self.setState(state: .Connected)
                             }
-                     });
+                     })
         }
     }
     
     func stopRecording() {
         if (mState == .StartRecording || mState == .Recording) {
-            mYICamera.stopRecording(success: nil, fail: nil);
+            _ = mYICamera.stopRecording(success: nil, fail: nil)
         }
     }
     
     func getStatus() -> State {
-        return mState;
+        return mState
     }
     
     func getIP() -> String {
-        return mIP;
+        return mIP
     }
     
     func getHost() -> String {
-        return mHost;
+        return mHost
     }
     
     override func onConnected() {
-        print("connect to camera: \(mIP)");
-        mTimer?.invalidate();
-        setState(state: .Connected);
-        mYICamera.getStatus(
+        print("connect to camera: \(mIP)")
+        mTimer?.invalidate()
+        setState(state: .Connected)
+        _ = mYICamera.getStatus(
             success: { status in
                 if (status == .Recording) {
-                    self.setState(state: .Recording);
+                    self.setState(state: .Recording)
                 }
             },
-            fail: nil);
+            fail: nil)
     }
     
     override func onRecordStarted() {
-        print("record started");
-        setState(state: .Recording);
+        print("record started")
+        setState(state: .Recording)
     }
     
     override func onRecordStopped() {
-        print("record stoped");
-        setState(state: .Connected);
+        print("record stoped")
+        setState(state: .Connected)
     }
     
-    override func onClosed(error error: YICameraSDKError?) {
-        print("disconnect from camera: \(mIP)");
-        setState(state: .Disconnected);
+    override func onClosed(error: YICameraSDKError?) {
+        print("disconnect from camera: \(mIP)")
+        setState(state: .Disconnected)
     }
     
     @objc private func onConnectTimeout() {
-        print("connect timeout");
-        mYICamera.disconnect();
+        print("connect timeout")
+        mYICamera.disconnect()
     }
     
     private func setState(state: State) {
         if (mState != state) {
-            let oldState = mState;
-            mState = state;
-            mListener?.onStateChanged(camera: self, newState: mState, oldState: oldState);
+            let oldState = mState
+            mState = state
+            mListener?.onStateChanged(camera: self, newState: mState, oldState: oldState)
         }
     }
 }
--- sdk/swift/samples/YI360Demo/CameraGridViewController.swift
+++ sdk/swift/samples/YI360Demo/CameraGridViewController.swift
@@ -15,122 +15,122 @@
 // THIS SOFTWARE OR ITS DERIVATIVES.
 //=============================================================================
 
-import UIKit;
+import UIKit
 
 class CameraGridViewController: UIViewController, UICollectionViewDataSource, CameraListener {
     @IBOutlet weak var mActionMenu: UIView!
 
-    var mCapacity = 0;
-    private var mScanCameraExitEvent = NSCondition();
-    private var mPendingCameras: [Camera] = [];
-    private var mCameras: [Camera] = [];
-    private var mCollectionView: UICollectionView!;
+    var mCapacity = 0
+    private var mScanCameraExitEvent = NSCondition()
+    private var mPendingCameras: [Camera] = []
+    private var mCameras: [Camera] = []
+    private var mCollectionView: UICollectionView!
     
     override func viewDidLoad() {
-        super.viewDidLoad();
+        super.viewDidLoad()
         
-        title = "YI360Demo";
-        navigationItem.hidesBackButton = true;
-        startScanCameras();
+        title = "YI360Demo"
+        navigationItem.hidesBackButton = true
+        startScanCameras()
     }
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        mCollectionView = collectionView;
-        return mCapacity;
+        mCollectionView = collectionView
+        return mCapacity
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cameraCell", for: indexPath) as! CameraCell;
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cameraCell", for: indexPath) as! CameraCell
         if (indexPath.item < mCameras.count) {
-            cell.setCamera(camera: mCameras[indexPath.item]);
+            cell.setCamera(camera: mCameras[indexPath.item])
         } else {
-            cell.setCamera(camera: nil);
+            cell.setCamera(camera: nil)
         }
-        return cell;
+        return cell
     }
     
     // This function will be invoked when camera state has been changed
     func onStateChanged(camera: Camera, newState: Camera.State, oldState: Camera.State) {
         if (newState == .Connected) {
-            removeFrom(camera: camera, queue: &mPendingCameras);
+            removeFrom(camera: camera, queue: &mPendingCameras)
             
             // do we have free slot?
             if (mCameras.count == mCapacity) {
-                return;
+                return
             }
             
             // add to mCameras queue
-            mCameras.append(camera);
+            mCameras.append(camera)
             
             // attach to cell
             if let cell = mCollectionView.cellForItem(at: IndexPath(index: mCameras.count - 1)) as? CameraCell {
-                cell.setCamera(camera: camera);
+                cell.setCamera(camera: camera)
             }
         } else if (newState == .Disconnected) {
             // remove from pending queue
-            removeFrom(camera: camera, queue: &mPendingCameras);
+            removeFrom(camera: camera, queue: &mPendingCameras)
             
             // remove from mCameras queue
             if let index = mCameras.index(where: {$0.getIP() == camera.getIP()}) {
                 if let cell = mCollectionView.cellForItem(at: IndexPath(index: index)) as? CameraCell {
-                    cell.setCamera(camera: nil);
+                    cell.setCamera(camera: nil)
                 }
-                mCameras.remove(at: index);
+                mCameras.remove(at: index)
             }
         }
     }
     
     @IBAction func onMenuBtnClicked(_ sender: Any) {
-        mActionMenu.isHidden = !mActionMenu.isHidden;
+        mActionMenu.isHidden = !mActionMenu.isHidden
     }
     
     @IBAction func onGestureRecognizer(_ sender: Any) {
         if (!mActionMenu.isHidden) {
-            mActionMenu.isHidden = true;
+            mActionMenu.isHidden = true
         }
     }
     
     @IBAction func onExitBtnClicked(_ sender: Any) {
-        (parent as! UINavigationController).popViewController(animated: true);
-        stopScanCameras();
+        (parent as! UINavigationController).popViewController(animated: true)
+        stopScanCameras()
     }
     
     @IBAction func onStartRecordingBtnClicked(_ sender: Any) {
-        mActionMenu.isHidden = true;
+        mActionMenu.isHidden = true
         
         // Start recording after 10 seconds.
-        let startTime = NSDate(timeIntervalSinceNow: 10);
+        let startTime = NSDate(timeIntervalSinceNow: 10)
         
         for i in 0..<mCameras.count {
-            self.mCameras[i].startRecording(startTime: startTime);
+            self.mCameras[i].startRecording(startTime: startTime)
         }
-        showMessageBox(message: "Recording will be started after 10 seconds");
+        showMessageBox(message: "Recording will be started after 10 seconds")
     }
     
     @IBAction func onStopRecordingBtnClicked(_ sender: Any) {
-        mActionMenu.isHidden = true;
+        mActionMenu.isHidden = true
         
         for i in 0..<mCameras.count {
-            mCameras[i].stopRecording();
+            mCameras[i].stopRecording()
         }
     }
     
     // Start camera scan thread
     private func startScanCameras() {
-        Thread(target: self, selector: #selector(scanCameras), object: nil).start();
+        Thread(target: self, selector: #selector(scanCameras), object: nil).start()
     }
     
     // Stop camera scan thread
     private func stopScanCameras() {
-        mScanCameraExitEvent.lock();
-        mScanCameraExitEvent.signal();
-        mScanCameraExitEvent.unlock();
+        mScanCameraExitEvent.lock()
+        mScanCameraExitEvent.signal()
+        mScanCameraExitEvent.unlock()
     }
     
     // Remove camera from camera queue
     private func removeFrom(camera: Camera, queue: inout [Camera]) {
         if let index = queue.index(where: {$0.getIP() == camera.getIP()}) {
-            queue.remove(at: index);
+            queue.remove(at: index)
         }
     }
     
@@ -138,23 +138,23 @@ class CameraGridViewController: UIViewController, UICollectionViewDataSource, Ca
         for i in 0..<ipList.count {
             // if we don't have free slot, do nothing.
             if (mCapacity == mCameras.count) {
-                return;
+                return
             }
             
-            let ip = ipList[i];
-            let host = ipList[i + 1];
-            print("found ip: \(ip), host: \(host)");
+            let ip = ipList[i]
+            let host = ipList[i + 1]
+            print("found ip: \(ip), host: \(host)")
 
             // check whether this is in pending queue or in final queue
-            var index = mPendingCameras.index(where: {$0.getIP() == ip});
+            var index = mPendingCameras.index(where: {$0.getIP() == ip})
             if (index == nil) {
-                index = mCameras.index(where: {$0.getIP() == ip});
+                index = mCameras.index(where: {$0.getIP() == ip})
                 if (index == nil) {
                     // new ip, create a camera, put to pending queue, and connect it.
-                    let camera = Camera(ip: ip, host: host);
-                    camera.setListener(listener: self);
-                    mPendingCameras.append(camera);
-                    camera.connect();
+                    let camera = Camera(ip: ip, host: host)
+                    camera.setListener(listener: self)
+                    mPendingCameras.append(camera)
+                    camera.connect()
                 }
             }
         }
@@ -162,30 +162,30 @@ class CameraGridViewController: UIViewController, UICollectionViewDataSource, Ca
     
     // Camera scan thread
     @objc private func scanCameras() {
-        mScanCameraExitEvent.lock();
+        mScanCameraExitEvent.lock()
         while (true) {
             DispatchQueue.main.async {
-                var ips = [String]();
+                var ips = [String]()
                 // If iPhone is used as wifi hotspot, the client ip range is 172.20.10.1~172.20.10.10
                 for i in 1...10 {
-                    ips.append("172.20.10.\(i)");
-                    ips.append("172.20.10.\(i)");
+                    ips.append("172.20.10.\(i)")
+                    ips.append("172.20.10.\(i)")
                 }
-                self.processIPList(ipList: ips);
+                self.processIPList(ipList: ips)
             }
             
             // wait for exit event or re-scan after 10 seconds
             if (mScanCameraExitEvent.wait(until: NSDate(timeIntervalSinceNow: 10) as Date)) {
                 // exit event is fired, exit
-                break;
+                break
             }
         }
-        mScanCameraExitEvent.unlock();
+        mScanCameraExitEvent.unlock()
     }
     
     private func showMessageBox(message: String) {
-        let alert = UIAlertView();
-        alert.message = message;
+        let alert = UIAlertView()
+        alert.message = message
         alert.addButton(withTitle: "Ok")
         alert.show()
     }
